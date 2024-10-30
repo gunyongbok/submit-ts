83adfcf8a0a698ee87f0deafe4c4492d079af6cf
gunyongbok <qhrrjsdyd123@gmail.com>
2024-10-30 14:44:53 +0900

    10.30

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="test">Hello, React</h1>
+        <p>반갑습니다.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
